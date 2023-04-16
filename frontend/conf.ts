00c18661fa85874204e05454ddc436554bed708a
azizcruz <learning094@gmail.com>
2023-04-16 14:58:15 +0200

    fix images not found

--- frontend/conf.ts
+++ frontend/conf.ts
@@ -1,3 +1,7 @@
 export const baseUrl = import.meta.env.DEV
   ? "http://web:8000"
   : "http://webserver:80";
+
+export const imageBaseUrl = import.meta.env.DEV
+  ? "http://web:8000"
+  : "http://0.0.0.0:1337";
